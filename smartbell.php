291c31dbd85d31217facdf353fb0d94efe69a21f
20212TIOT0111 <vitoria.l.s.s2018@gmail.com>
2024-06-07 13:35:34 -0300

    first commit

---
+++ smartbell.php
@@ -0,0 +1,59 @@
+<?php
+ 
+/*
+ * O seguinte codigo abre uma conexao com o BD e adiciona um produto nele.
+ * As informacoes de um produto sao recebidas atraves de uma requisicao POST.
+ */
+
+// conexão com bd
+//require_once('conexao_db.php');
+
+// array de resposta
+$resposta = array();
+
+	
+// Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
+// A criacao de um produto precisa dos seguintes parametros:
+// nome - nome do produto
+// preco - preco do produto
+// descricao - descricao do produto
+// img - imagem do produto
+if (isset($_POST['filename']) && isset($_POST['mimetype']) && isset($_POST['data'])) {
+	
+	// Aqui sao obtidos os parametros
+	$filename = $_POST['filename'];
+	$mimetype = $_POST['mimetype'];
+	$data = $_POST['data'];
+	
+	
+	$client_id="1c36a4fcfc1c1a8";
+	$pvars   = array('image' => $data);
+	$timeout = 30;
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+	curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+	$out = curl_exec($curl);
+	curl_close ($curl);
+	$pms = json_decode($out,true);
+	$img_url=$pms['data']['link'];
+	
+} else {
+	// Se a requisicao foi feita incorretamente, ou seja, os parametros 
+	// nao foram enviados corretamente para o servidor, o cliente 
+	// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+	// motivo da falha.
+	$resposta["sucesso"] = 0;
+	$resposta["erro"] = "Campo requerido nao preenchido";
+}
+
+
+// Fecha a conexao com o BD
+//$db_con = null;
+
+// Converte a resposta para o formato JSON.
+echo $out;
+?>
