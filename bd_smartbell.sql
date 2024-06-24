9c747d998052d23d8d8f26da0b276193ad55936e
SmartBelliot <168777804+SmartBelliot@users.noreply.github.com>
2024-06-24 11:51:41 -0300

    mudei o banco de dados

--- bd_smartbell.sql
+++ bd_smartbell.sql
@@ -1,8 +1,6 @@
-
-
 CREATE TABLE public.visitas (
 	id serial4 NOT NULL,
-	img varchar(200) NOT NULL,
+	img text NOT NULL,
 	data_criacao timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT visitas_pk PRIMARY KEY (id)
 );
